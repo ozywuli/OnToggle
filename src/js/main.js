0ce9c649b98895da902c83612624ce4b5dac08aa
ozywuliwq <ozy.wu.li@gmail.com>
2017-11-05 17:36:08 -0800

    updates

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,16 @@
-let x = 300;
+$('.js-toggler').on('click', function(e) {
+    e.preventDefault();
+    let target = $(this).attr('data-toggler-target');
+
+    $('.js-toggler-target').not( $(`.${target}`) ).removeClass('is-revealed');
+
+    $(`.${target}`).toggleClass('is-revealed');
+    
+});
+
+
+$(document).on('click', function(e) {
+    if ( !$(e.target).closest('.js-toggler, .js-toggler-target').length ) {
+        $('.js-toggler-target').removeClass('is-revealed');
+    }
+});
